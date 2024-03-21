760980f262f9a2b1e85c51c116c0cd210ad6409a
ANA MARIA VIEIRA BEZERRA <ana_m_bezerra@estudante.sesisenai.org.br>
2024-03-20 21:19:58 -0300

    Criação de botões customizados

--- src/componentes/BotaoCustomizado/BotaoCustomizado.jsx
+++ src/componentes/BotaoCustomizado/BotaoCustomizado.jsx
@@ -1,7 +1,20 @@
 import './BotaoCustomizado.css';
 
-const BotaoCustomizado = () => {
-    return <button></button>;
+const BotaoCustomizado = (props) => {
+    const classe = ["botao-customizado"];
+
+    switch(props.tipo){
+        case "primario":
+            classe.push("botao-customizado-primario");
+            break;
+            case "secundario":
+            classe.push("botao-customizado-secundario");
+            break;
+    }
+    
+
+    return <button className={classe.join(" ")}>{props.children}</button>;
 
 };
+
 export default BotaoCustomizado;
--- src/componentes/Principal/Principal.jsx
+++ src/componentes/Principal/Principal.jsx
@@ -1,7 +1,15 @@
+import BotaoCustomizado from '../BotaoCustomizado/BotaoCustomizado';
 import './Principal.css';
 
 const Principal = () => {
-  return <main>Principal</main>;
+  return (
+  <main>
+    <BotaoCustomizado tipo="primario">Salvar</BotaoCustomizado> 
+    <BotaoCustomizado tipo="secundario">Excluir</BotaoCustomizado>
+    <BotaoCustomizado>Cancelar</BotaoCustomizado>
+    
+    </main>
+  );
 };
 
 export default Principal;
